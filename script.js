625b9fce7037b78045f609d6d59f723bc544ea4a
TopMyster <topmyster@outlook.com>
2025-09-25 19:41:49 -0500

    Updates the ai content

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ async function submition() {
         messages: [
           {
             role: "user",
-            content: `Build a professional website based on ${transcript}. Make it as professional and nice looking. Give only the code as your response nothing else. Make there a thing banner that is the accent colour of the website you make that says Made in Forma. Rid the site of bugs and clutter ness. The site must look very human as well. The site must be beautiful as well. Also add a comment in the beginning of the code, "Made in Forma"`,
+            content: `Build a professional website based on ${transcript}. Make it as professional and nice looking. Give only the code as your response nothing else. Make there a thing banner that is the accent colour of the website you make that says Made in Forma. Rid the site of bugs and clutter ness. The site must look very human as well. The site must be beautiful as well. Also add a comment in the beginning of the code, "Made in Forma". Make all the site features work like when they want it to lead to a different page or a email sender.`,
           },
           {
             role: "user",
